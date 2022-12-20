7f7df8f7681fe44af2459f711507a10214021f40
Julia Moore <peony.the.pretty@gmail.com>
2022-12-20 10:53:15 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,55 @@
+console.log(`connected`);
+
+let namePower = document.getElementById(`name-power`);
+let nameInput = document.getElementById(`name-input`);
+let powerInput = document.getElementById(`power-input`);
+let getAll = document.getElementById(`get-all`);
+let forChars = document.getElementById(`for-chars`);
+let deleteForm = document.getElementById(`delete-form`);
+let deleteButton = document.getElementById(`delete-button`);
+let deleteDB = document.getElementById(`delete-db`);
+
+const baseURL = `http://localhost:4000`;
+
+function clearChars() {
+  forChars.innerHTML = ``;
+}
+
+const getAllChars = () => {
+  clearChars();
+  axios.get(`${baseURL}/api/character`).then((res) => {
+    for (i = 0; i < res.data.length; i++) {
+      let charCard = document.createElement(`div`);
+      charCard.textContent =
+        `${res.data[i].name}` + ` ` + `${res.data[i].power}`;
+      forChars.appendChild(charCard);
+    }
+  });
+};
+
+getAll.addEventListener(`click`, getAllChars);
+
+const createNewChar = (event) => {
+  event.preventDefault();
+
+  let body = {
+    name: nameInput.value,
+    power: powerInput.value,
+  };
+
+  axios.post(`${baseURL}/api/character`, body).then((res) => {
+    console.log(res.data);
+  });
+};
+
+namePower.addEventListener(`submit`, createNewChar);
+
+function deleteChar(id) {
+    id.preventDefault()
+  id === deleteDB.value;
+  return axios.delete(`${baseURL}/api/character/${id}`).then((res) => {
+    res.data;
+  });
+}
+
+deleteButton.addEventListener(`submit`, deleteChar);
---
+++ server/index.js
@@ -0,0 +1,47 @@
+let express = require(`express`);
+let cors = require(`cors`);
+
+let app = express();
+
+app.use(express.json());
+app.use(cors());
+
+let character = [
+  {
+    name: `Harry Potter`,
+    power: 1000,
+    id: 0,
+  },
+  {
+    name: `Voldemort`,
+    power: 999,
+    id: 1,
+  },
+  {
+    name: `Hermione`,
+    power: 1000,
+    id: 2,
+  },
+];
+
+let id = 3;
+
+app.post(`/api/character`, (req, res) => {
+  let newChar = { ...req.body, id };
+  character.push(newChar);
+  res.status(200).send(character);
+  id++;
+});
+
+app.get(`/api/character`, (req, res) => {
+  res.status(200).send(character);
+});
+
+app.delete(`/api/character/:id`, (req, res) => {
+  const { id } = req.params;
+  const index = character.findIndex((char) => char.id === id);
+  character.splice(index, 1);
+  return res.send();
+});
+
+app.listen(4000, console.log(`The server is running on 4000`));
